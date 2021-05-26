12f3643875f81b0dda08fe65a6f050fb395b8be0
Kevin van Nieukerke <k.v.nieukerke@gmail.com>
2021-05-26 15:49:45 +0200

    fix for theme update variables not generating correct css

--- functions.php
+++ functions.php
@@ -8,17 +8,7 @@
 
 		add_action('after_setup_theme', 'PREFIX_check_theme_version');
 		function PREFIX_check_theme_version() {
-			
-			$current_version = wp_get_theme()->get('Version');
-			$old_version = get_option( 'PREFIX_theme_version' );
-			
-			if ($old_version !== $current_version) {
-				// do some cool stuff
-				generate_variables_scss('20');
-				
-				// update not to run twice
-				update_option('PREFIX_theme_version', $current_version);
-			}
+			generate_variables_scss('20');
 		}
 	
 		add_action( 'acf/save_post', 'generate_variables_scss', 20 );
